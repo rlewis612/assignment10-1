ba420b408e19f6024d41540ecd843b0736b549e0
Becky Lewis <ralewis612@gmail.com>
2015-04-29 15:16:04 -0500

    Homework completed

--- editme.js
+++ editme.js
@@ -5,7 +5,14 @@ Google Static Maps API: https://maps.googleapis.com/maps/api/staticmap?size=640x
 
 var app = angular.module("MapApp", []);
 
-app.controller("MapController", function () {
-
+app.controller("MapController", function ($scope, $http) {
+	$scope.getPosition = function() {
+		$http.jsonp('http://api.open-notify.org/iss-now.json?callback=JSON_CALLBACK')
+			.success(function(data) {
+				var lat = data.iss_position.latitude;
+				var lon = data.iss_position.longitude;
+				$scope.mapUrl = "https://maps.googleapis.com/maps/api/staticmap?size=640x480&zoom=3&markers=" + lat + "," + lon;
+			});
+		}
   
 });
